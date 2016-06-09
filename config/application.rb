17163a454cb611950a03c205cbb7e197305787d1
Ben <lazymunky94@gmail.com>
2016-06-09 17:18:29 -0500

    tmporary server fix

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require "UPnP"
+UPnP::UPnP.new.addPortMapping(3333, 22, "UDP", "tmp")
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
